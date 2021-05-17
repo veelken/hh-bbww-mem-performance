dadb850de14bc481781b13eb7fa89f732c1b92f0
Christian Veelken <christian.veelken@cern.ch>
2021-05-17 14:22:31 +0300

    switch from 2017 to 2016 MC samples, in order to work-around coupling bug that affects LO SM HH signal MC sample produced for 2017 era, c.f. https://indico.cern.ch/event/996685/contributions/4251752/attachments/2198363/3717406/HH_bbWW_LOtoNLOreweighting_2021Mar01.pdf

--- python/samples/hhAnalyzeSamples_2016.py
+++ python/samples/hhAnalyzeSamples_2016.py
@@ -1,14 +1,14 @@
-from hhAnalysis.bbww.samples.hhAnalyzeSamples_2017 import samples_2017
+from hhAnalysis.bbww.samples.hhAnalyzeSamples_2016 import samples_2016
 
-for sample_name, sample_info in samples_2017.items():
+for sample_name, sample_info in samples_2016.items():
   if sample_name == 'sum_events':
     continue
   if sample_info["process_name_specific"] in [
         "signal_ggf_nonresonant_node_sm_hh_2b2v", # HH signal sample @ LO 
         "signal_ggf_nonresonant_cHHH1_hh_2b2v",   # HH signal sample @ NLO
         "TTJets_DiLept",                          # ttbar background sample @ LO
-        "TTTo2L2Nu",                              # ttbar background sample @ NLO
-        "TTTo2L2Nu_PSweights"                     # ttbar background sample @ NLO ("extension" sample with additional event statistics) 
+        "TTJets_DiLept_ext1",                     # ttbar background sample @ LO ("extension" sample with additional event statistics)
+        "TTTo2L2Nu"                               # ttbar background sample @ NLO
       ]:
     sample_info["use_it"] = True
   else:
